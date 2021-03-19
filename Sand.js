76f71c3206299e13e84474a50514769e82ca5b22
OjalKamble <75155992+OjalKamble@users.noreply.github.com>
2021-03-19 16:40:58 +0530

    Update Sand.js

--- Sand.js
+++ Sand.js
@@ -5,7 +5,7 @@ class Sand {
         'friction':5,
         'density':1 
       }
-      this.body = Bodies.circle(x,y,radius);
+      this.body = Bodies.circle(x,y,radius,options);
       this.radius = radius;
       
       World.add(world, this.body);
